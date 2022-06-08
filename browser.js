9212ec1807b6c400cbb5b128b83a0caff499c907
Lovro <hodak.lovro@gmail.com>
2022-06-08 18:26:23 +0200

    redeploy2

--- browser.js
+++ browser.js
@@ -2,5 +2,6 @@ const puppeteer = require('puppeteer')
 
 module.exports = () => puppeteer.launch({
     headless: true,
-    defaultViewport: {width: 1300, height: 1000}
+    defaultViewport: {width: 1300, height: 1000},
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
 })
