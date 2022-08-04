572d66eb33475f482288996c380f249e79a2ab72
Lovro <hodak.lovro@gmail.com>
2022-08-04 11:39:33 +0200

    forgot to change headless, should work now

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 module.exports = () => puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: {width: 1300, height: 1000},
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
 })
